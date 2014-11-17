9f7479e7a6c707261a6252ed4d9fdd2975ba28d8
abdelhak <abdelhak@abdelhak-HP>
2014-11-17 15:59:14 +0100

    Commit Final de va Version 4.0.0
    	Ajout de la documentation

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,8 +1,5 @@
 package com.mygdx.game.desktop;
 
-import java.nio.file.Path;
-import java.nio.file.spi.FileTypeDetector;
-
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.IceBox;
